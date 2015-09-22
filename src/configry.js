825e939cf4079f14fd356753e0058f54089eacd5
Amanpreet Singh <apsdehal@gmail.com>
2015-09-22 13:31:10 +0530

    Fix #3 by explicitly checking the existence of nested object

--- src/configry.js
+++ src/configry.js
@@ -79,7 +79,11 @@ Configry.prototype.set = function (key, value, per) {
     var keys=key.split('.');
     // a.b.c.d=value => {a:{b:{c:{d:value}}}}
     for (var i = 0, tmp = this.config; i < keys.length - 1; i++) {
-       tmp = tmp[keys[i]] = {};
+      if (typeof tmp[keys[i]] == 'object') {
+        tmp = tmp[keys[i]];
+      } else {
+        tmp = tmp[keys[i]] = {};
+      }
     }
     tmp[keys[i]] = value;
   }
